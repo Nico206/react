f8c8df310771746748400ac9c63187754c6c67fe
SamanthaLopez1021 <lopezeimmy402@gmail.com>
2023-11-02 08:30:48 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router,  Route, Routes} from 'react-router-dom'
+import Login from './paginas/auth/login';
+import Dashboard from './paginas/auth/dashboard';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path="/"exact element={<Login/>}></Route>
+          <Route path="/dashboard"exact element={<Dashboard/>}></Route>  
+          
+        </Routes>
+      </Router>
+    </Fragment>
   );
 }
 
